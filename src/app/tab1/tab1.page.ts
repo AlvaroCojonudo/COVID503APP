ef57a7c8c6ed4606e40015c796f84dec8732cc5d
alvarocojonudo <alvarocojonudo@gmail.com>
2020-05-19 19:14:24 -0600

    parte SV completada

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,7 +7,7 @@ import { Covid19Service } from '../services/covid19.service';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
-  public date: Date = new Date();
+  public date: string;
   public confirmed;
   public deaths;
   public recovered;
@@ -17,74 +17,52 @@ export class Tab1Page implements OnInit {
       let casesConfirmeds = [];
       let datesConfirmeds = [];
       let casesDeaths = [];
-      this.date = new Date(datos[Object.keys(datos).length - 1].Date);
+      let casesActive = [];
+      let casesRecovered = [];
+      this.date = new Date(datos[Object.keys(datos).length - 1].Date).toLocaleTimeString("es-SV", { year: "numeric", month: "short", day: "numeric" });
       for (let i = 0; i <= Object.keys(datos).length - 1; i++) {
-        //casesConfirmeds.push(datos[i].Confirmed);
-        //casesDeaths.push(datos[i].Deaths);
-        let mes: string;
-        switch (new Date(datos[i].Date).getUTCMonth() + 1) {
-          case 1:
-            mes = "Ene";
-            break;
-          case 2:
-            mes = "Feb";
-            break;
-          case 3:
-            mes = "Mar";
-            break;
-          case 4:
-            mes = "Abr";
-            break;
-          case 5:
-            mes = "May";
-            break;
-          case 6:
-            mes = "Jun";
-            break;
-          case 7:
-            mes = "Jul";
-            break;
-          case 8:
-            mes = "Ago";
-            break;
-          case 9:
-            mes = "Sep";
-            break;
-          case 10:
-            mes = "Oct";
-            break;
-          case 11:
-            mes = "Nov";
-            break;
-          case 12:
-            mes = "Dic";
-            break;
-
-          default:
-            mes = "-";
-            break;
-        }
-        let dateString: string = new Date(datos[i].Date).getUTCDate().toString() + "/" + mes;
+        let dateString: string = new Date(datos[i].Date).toLocaleTimeString("es-SV", { month: "short", day: "numeric" });
         casesConfirmeds.push({ x: dateString, y: datos[i].Confirmed });
         casesDeaths.push({ x: dateString, y: datos[i].Deaths });
-        datesConfirmeds.push(dateString);
+        casesActive.push({ x: dateString, y: datos[i].Active });
+        casesRecovered.push({ x: dateString, y: datos[i].Recovered });
+        datesConfirmeds.push(dateString.substring(0, dateString.length - 10));
       }
-      //console.log(casesDeaths);
       var ctx = document.getElementById('myChart');
       var myChart = new Chart(ctx, {
         type: 'line',
         data: {
           labels: datesConfirmeds,
-          datasets: [{
-            label: 'Casos Confirmados',
-            data: casesConfirmeds,
-            fill: true,
-            backgroundColor: 'rgba(44, 242, 74, 0.4)',
-            borderColor: 'rgba(44, 242, 74, 1)',
-            borderWidth: 1,
-            pointBorderWidth: 0,
-            pointStyle: "line"
-          }]
+          datasets: [
+            {
+              label: 'Muertes',
+              backgroundColor: 'rgba(255, 15, 15, 0.8)',
+              data: casesDeaths,
+              fill: true,
+              pointRadius: 0
+            },
+            {
+              label: 'Recuperados',
+              fill: true,
+              backgroundColor: 'rgba(15, 235, 255, 0.6)',
+              data: casesRecovered,
+              pointRadius: 0
+            },
+            {
+              label: 'Activos',
+              fill: true,
+              backgroundColor: 'rgba(15, 119, 255, 0.8)',
+              data: casesActive,
+              pointRadius: 0
+            },
+            {
+              label: 'Confirmados',
+              fill: true,
+              backgroundColor: 'rgba(33, 219, 0, 1)',
+              data: casesConfirmeds,
+              pointRadius: 0
+            }
+          ]
         },
         options: {
           scales: {
